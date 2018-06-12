353b4c48f4f4681b4ab1c68a12d2d3833c76dc80
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-06-12 23:14:39 +0200

    Remove excessive hibernate logging

--- src/main/java/hibernate/HibernateHelper.java
+++ src/main/java/hibernate/HibernateHelper.java
@@ -6,11 +6,17 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HibernateHelper {
 
     private static SessionFactory sessionFactory;
 
     public static void prepareSessionFactory() {
+        // Hide Hibernate INFO console printouts
+        Logger.getLogger("org.hibernate").setLevel(Level.SEVERE);
+
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
         sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
     }
