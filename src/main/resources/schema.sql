7fe11d205318a7d5dab2c5f29e5c5459f11c9289
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-06-12 23:29:45 +0200

    Update sql schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,13 @@
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
+
+CREATE TABLE authors (
+    id BIGINT AUTO_INCREMENT,
+    name VARCHAR(100),
+    surname VARCHAR(255),
+
+    PRIMARY KEY(id)
+);
 
 CREATE TABLE books (
     id BIGINT AUTO_INCREMENT,
@@ -6,12 +15,20 @@ CREATE TABLE books (
     number_of_pages INT,
     has_hard_cover BIT(1),
     release_date DATE,
+    author_id BIGINT NOT NULL,
 
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY (author_id) REFERENCES authors (id)
 );
 
-INSERT INTO books
-        (title, number_of_pages, has_hard_cover, release_date)
+INSERT INTO authors
+        (id, name, surname)
     VALUES
-        ('Just Hibernate', 140, b'0', '2014-06-10'),
-        ('Hibernate: A Developers Notebook', 192, b'1', '2004-05-20');
+        (1, 'James', 'Elliott'),
+        (2, 'Madhusudhan', 'Konda');
+
+INSERT INTO books
+        (title, number_of_pages, has_hard_cover, release_date, author_id)
+    VALUES
+        ('Hibernate: A Developers Notebook', 192, b'1', '2004-05-20', 1),
+        ('Just Hibernate', 140, b'0', '2014-06-10', 2);
