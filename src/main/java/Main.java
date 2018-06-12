002576ae9a987b3f58b6e08c5724279858dd0946
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-06-12 23:30:19 +0200

    Update Main to show one to one relation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
 import hibernate.HibernateHelper;
+import hibernate.entities.Author;
 import hibernate.entities.Book;
-import hibernate.examples.EntityManipulationExamples;
 import org.hibernate.Session;
 
-import java.util.List;
 
 public class Main {
 
@@ -12,62 +11,21 @@ public class Main {
 
         Session session = HibernateHelper.openSession();
 
-        Book book;
-        List<Book> books;
+        Book book = session.get(Book.class, 1L);
+        System.out.println("Book 1 and its main author");
+        System.out.println(book);
+        System.out.println(book.getMainAuthor());
+        System.out.println("\n");
 
-        EntityManipulationExamples examples = new EntityManipulationExamples(session);
-
-        book = examples.findBookById(1L);
-        displayResult("Book of id 1", book);
-
-        books = examples.findAllBooks();
-        displayResult("All books", books);
-
-        books = examples.findBooksByPageCountGraterThan150();
-        displayResult("Books with more than 150 pages", books);
-
-        books = examples.findBooksByParametrizedPageCount(100);
-        displayResult("Books with more than 100 pages", books);
-
-        book = examples.createNewBook();
-        displayResult("Newly created book", book);
-
-        books = examples.findAllBooks();
-        displayResult("All books", books);
-
-        book = examples.updateExistingBook(book);
-        displayResult("Updated book", book);
-
-        books = examples.findAllBooks();
-        displayResult("All books", books);
-
-        book = examples.replaceExistingBook(book.getId());
-        displayResult("Replaced book", book);
-
-        books = examples.findAllBooks();
-        displayResult("All books", books);
-
-        examples.deleteBook(book.getId());
-        System.out.println("Deleting book " + book.getId());
-
-        books = examples.findAllBooks();
-        displayResult("All books", books);
+        Author author = session.get(Author.class, 2L);
+        System.out.println("Author 2 and his first book");
+        System.out.println(author);
+        System.out.println(author.getFirstBook());
+        System.out.println("\n");
 
         session.close();
 
         HibernateHelper.closeSessionFactory();
-    }
-
-    private static void displayResult(String label, Book book) {
-        System.out.println(label);
-        System.out.println(book);
-        System.out.println("\n");
-    }
-
-    private static void displayResult(String label, List<Book> books) {
-        System.out.println(label);
-        books.forEach(System.out::println);
-        System.out.println("\n");
     }
 
 }
