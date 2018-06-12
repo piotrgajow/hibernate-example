62f54775bf9bcd6cb7ce9bfc9e28fb30d1ab2b81
Piotr Gajowniczek <piotrgajow@gmail.com>
2018-06-12 22:23:53 +0200

    Add db schema

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+    id BIGINT AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    number_of_pages INT,
+    has_hard_cover BIT(1),
+    release_date DATE,
+
+    PRIMARY KEY(id)
+);
+
+INSERT INTO books
+        (title, number_of_pages, has_hard_cover, release_date)
+    VALUES
+        ('Just Hibernate', 140, b'0', '2014-06-10'),
+        ('Hibernate: A Developers Notebook', 192, b'1', '2004-05-20');
